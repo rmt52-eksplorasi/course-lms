26c5e708e1b44a9df450d6fe0caf9ed5b9becce9
ForestoAY <forestoay@gmail.com>
2024-10-24 16:08:04 +0700

    init project

---
+++ client/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Welcome to Course LMS</h1>
+    <p>This is the main page of the Course Learning Management System.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #2c3e50;
+}
+p {
+  font-size: 18px;
+}
+</style>
